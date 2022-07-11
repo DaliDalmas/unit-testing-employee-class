434c70cdc5dc64ca386540fb962649a6890b1ec3
Dalmas.Otieno <dalmas.otieno@GLB-1602019-1779.local>
2022-07-11 23:15:20 +0300

    setting up the class

---
+++ src/employee.py
@@ -0,0 +1,28 @@
+from datetime import datetime 
+class Employee:
+    """
+    """
+    def __init__(self, first_name, middle_name, sur_name):
+        """
+        """
+        pass
+
+    def salary_increament(self, increment):
+        """
+        """
+        pass
+
+    def leave_application(self, start_date, end_date, leave_type):
+        """
+        """
+        pass
+
+    def update_leave(self, leave_id):
+        """
+        """ 
+        pass
+
+    def display_details(self):
+        """
+        """
+        pass
---
+++ test/test_employee.py
@@ -0,0 +1,7 @@
+import pytest
+from employee import Employee
+
+class TestSalaryIncrement:
+    @pytest.xfail
+    def test_salary_increment_on_negative_increment():
+        

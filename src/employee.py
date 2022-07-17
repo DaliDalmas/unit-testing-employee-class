b765763c17c4354004b344287bf52629f55a538e
Dalmas.Otieno <dalmas.otieno@GLB-1602019-1779.local>
2022-07-17 18:00:22 +0300

    completed the employee and manager classes

--- src/employee.py
+++ src/employee.py
@@ -20,23 +20,34 @@ class Employee:
         Middle name: {self.middle_name}
         Sur name: {self.sur_name}
         salary: {self._salary}
+        Date of Births: {self._date_of_birth}
+        __________________________
         """
+
     def __repr__(self):
-        return(f'Employee("{self.first_name}", "{self.middle_name}", "{self.sur_name}", {self._salary})')
+        return(f'Employee("{self.first_name}", "{self.middle_name}", "{self.sur_name}", {self._salary}, {self._date_of_birth})')
 
     @property
     def salary(self):
         return self._salary
 
-    @property.setter
+    @salary.setter
     def salary(self,initial_salary):
-        if initial_salary<Employee.MIN_SALARY:
+        if initial_salary < Employee.MIN_SALARY:
             raise Exception("Salary less than minimum salary")
         self._salary = initial_salary
 
-    @classmethod
-    def increase_salary(cls):
-        return cls._salary*((100+cls._salary_percentage_increment)/100)
+    def increase_salary(self):
+        return self._salary*((100+self._salary_percentage_increment)/100)
 
     def get_age(self):
-        return datetime.now - self._date_of_birth
+        return datetime.today().year - datetime.strptime(self._date_of_birth, '%Y-%M-%d').date().year
+
+def main():
+    print("Example employee class")
+    emp = Employee("Dalmas", "DaliCodes", "Otieno", 6000, "1992-03-23")
+    print(emp)
+    print(f"The age of employee is {emp.get_age()}")
+
+if __name__=="__main__":
+    main()
--- src/manager.py
+++ src/manager.py
@@ -8,16 +8,17 @@ class Manager(Employee):
     def __init__(self, first_name, middle_name, sur_name, initial_salary, dob, role="manager", percentage_increment=5, team="admin"):
         self.role = role
         self._salary_percentage_increment = percentage_increment
+        self.team = team
         self.my_budget_stats = None
 
         Employee.__init__(self, first_name, middle_name, sur_name, initial_salary, dob)
-        self._salary = Employee.increase_salary()
+        self._salary = Employee.increase_salary(self)
 
     @property
     def salary_percentage_increment(self):
         return self._salary_percentage_increment
 
-    @property.setter
+    @salary_percentage_increment.setter
     def salary_percentage_increment(self, percentage_increment):
         if percentage_increment<0:
             raise Exception("Invalid salary increment")
@@ -38,3 +39,30 @@ class Manager(Employee):
         }
 
         return self.my_budget_stats
+
+    def __str__(self):
+        return f"""
+        Employee
+        ___________________________
+        First name: {self.first_name}
+        Middle name: {self.middle_name}
+        Sur name: {self.sur_name}
+        salary: {self._salary}
+        Date of Birth: {self._date_of_birth}
+        Role: {self.role}
+        team:{self.team}
+        __________________________
+        """
+
+    def __repr__(self):
+        return f'Manager("{self.first_name}", "{self.middle_name}", "{self.sur_name}", {self.salary}, "{self._date_of_birth}", "{self.role}", {self._salary_percentage_increment}, "{self.team}")'
+
+
+def main():
+    print("Example employee class")
+    emp = Manager("Dalmas", "DaliCodes", "Otieno", 6000, "1992-03-23", "CTO", 50, "Technology")
+    print(emp)
+    print(f"The age of employee is {emp.get_age()}")
+
+if __name__=="__main__":
+    main()

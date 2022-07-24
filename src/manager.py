959646866c4da53c6419c2e9f87d9e4b3376ead5
Dalmas.Otieno <dalmas.otieno@GLB-1602019-1779.local>
2022-07-24 13:32:59 +0300

    complete

--- src/manager.py
+++ src/manager.py
@@ -2,13 +2,32 @@ from src.employee import Employee
 
 
 class Manager(Employee):
+    """
+    
+    """
 
     def __init__(self, first_name: str, middle_name: str, sur_name: str, initial_salary: int, dob: str,
                  role :str="manager", percentage_increment:int=5, team:str="admin"):
         self.role: str= role
         self._salary_percentage_increment: str = percentage_increment
         self.team: str = team
-        self.my_budget_stats: dict = {}
+
+        if isinstance(role, str):
+            pass
+        else:
+            raise TypeError("The type of role should be string")
+        
+        if isinstance(percentage_increment, int):
+            pass
+        elif isinstance(percentage_increment, float):
+            pass
+        else:
+            raise TypeError("percentage increment should be int or float")
+
+        if isinstance(team, str):
+            pass
+        else:
+            raise TypeError("The type of role should be string")
 
         Employee.__init__(
             self,
@@ -17,6 +36,7 @@ class Manager(Employee):
             sur_name,
             initial_salary,
             dob)
+
         self._salary = Employee.increase_salary(self, self._salary_percentage_increment)
 
     @property
@@ -28,32 +48,6 @@ class Manager(Employee):
         if percentage_increment < 0:
             raise Exception("Invalid salary increment")
         self._salary_percentage_increment = percentage_increment
-
-    def create_budget_stats(
-            self,
-            prev_budget: float,
-            prev_funding: float,
-            prev_expenditure: float,
-            next_budget: float,
-            next_funding: float):
-
-        self.my_budget_stats = {
-            "prev_budget": prev_budget,
-            "prev_funding": prev_funding,
-            "prev_expenditure": prev_expenditure,
-            "next_budget": next_budget,
-            "next_funding": next_funding,
-            "budge_percentage_change":
-            (prev_budget-next_budget)*100/prev_budget,
-            "prev_budget_percentage_not_funded":
-            (prev_budget-prev_funding)*100/prev_budget,
-            "percentage_surplus_funding":
-            (prev_funding-prev_expenditure)*100/prev_funding,
-            "funding_percentage_change":
-            (prev_funding-next_funding)*100/prev_funding
-        }
-
-        return self.my_budget_stats
 
     def __str__(self):
         return f"""
@@ -85,10 +79,6 @@ def main():
 
     print(emp)
     print(f"The age of employee is {emp.get_age()}")
-
-    print("Creating budget statistics")
-    print(emp.create_budget_stats(20000, 19000, 14569, 30000, 23140))
-
 
 if __name__ == "__main__":
     main()
--- tests/test_manager.py
+++ tests/test_manager.py
@@ -4,17 +4,25 @@ from src import manager
 
 class TestManagerObjectCreation:
 
-    def test_manager_object_creation_on_missing_arguments(self):
-        pass
-
     def test_manager_object_creation_on_wrong_argument_types(self):
-        pass
+        # test manager creation on role
+        with pytest.raises(TypeError):
+            man = manager.Manager('Anna', 'Mary', 'Nakato', 4000, '1995-07-17', 909, 50, 'data')
+        # test manager creation on percentage_increment
+        with pytest.raises(TypeError):
+            man = manager.Manager('Anna', 'Mary', 'Nakato', 4000, '1995-07-17', 'data lead', 'fifty', 'data')
+        # test manager creatioin on team
+        with pytest.raises(TypeError):
+            man = manager.Manager('Anna', 'Mary', 'Nakato', 4000, '1995-07-17', 'data lead', 50, 30)
 
-class TestCreateBudgetStats:
-
-    def test_create_budget_stats_on_wrong_argument_types(self):
-        pass
-
-    def test_create_budget_stats_on_missing_arguments(self):
-        pass
-
+    def test_manager_object_creation_on_right_argument_types(self):
+        # test manager creation on role
+        man = manager.Manager('Anna', 'Mary', 'Nakato', 4000, '1995-07-17', 'data lead', 50, 'data')
+        assert man.first_name == 'Anna'
+        assert man.middle_name == 'Mary'
+        assert man.sur_name == 'Nakato'
+        assert man._salary == 6000
+        assert man._date_of_birth == '1995-07-17'
+        assert man.role == 'data lead'
+        assert man._salary_percentage_increment == 50
+        assert man.team == 'data'
